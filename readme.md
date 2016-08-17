0fb797b50b385819b352aa524df2b7e6392fb08e
Kendrick Williams <williams.kendrick@gmail.com>
2016-08-17 12:49:36 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+## angular-2-form-example
+> This project is generated with [yo angular2-typescript generator](https://github.com/shibbir/generator-angular2-typescript) version 0.6.2.
+
+## Installation
+
+```bash
+$ npm install typescript -g
+$ npm start
+```
+
+## Production Build
+```bash
+$ npm run build
+```
+
+## Running Tests
+```bash
+$ npm test
+```
+
+## License
+<a href="https://opensource.org/licenses/BSD-2-Clause-FreeBSD">BSD-2-Clause-FreeBSD License</a>
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {routing, appRoutingProviders} from './app.routing';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing
+    ],
+    providers: [appRoutingProviders],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
